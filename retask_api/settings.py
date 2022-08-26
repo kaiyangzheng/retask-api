ccaede882e61e1f8c966cda4525a4cb3efd25f1e
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-08-26 00:28:24 -0700

    cors

--- retask_api/settings.py
+++ retask_api/settings.py
@@ -180,7 +180,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://localhost:8080',
     'https://retasker.netlify.app',
-    'https://retask.xyz'
+    'https://retask.xyz',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
